81c126eac4d7a61d0abc8bb1bcb1e9d1504512af
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-06 13:51:48 -0300

    feat: trigger destroy

--- certificate.tf
+++ certificate.tf
@@ -2,7 +2,7 @@ resource "aws_acm_certificate" "certificate_examples_subdomains" {
   for_each          = var.example_subdomains
   domain_name       = "${each.value.name}.${var.default_example_domain}"
   validation_method = "EMAIL"
-  provider          = aws.us_east-1
+  provider          = aws.us_east-12
 }
 
 resource "aws_acm_certificate_validation" "certificate_examples_validations" {
