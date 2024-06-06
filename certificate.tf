41feeea6437cb2ccfa9b31b06d8d4a7a69483ea4
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-06-06 14:17:17 -0300

    feat: add alias

--- certificate.tf
+++ certificate.tf
@@ -2,11 +2,9 @@ resource "aws_acm_certificate" "certificate_examples_subdomains" {
   for_each          = var.example_subdomains
   domain_name       = "${each.value.name}.${var.default_example_domain}"
   validation_method = "EMAIL"
-  provider          = aws.us_east-2
 }
 
 resource "aws_acm_certificate_validation" "certificate_examples_validations" {
   for_each        = aws_acm_certificate.certificate_examples_subdomains
   certificate_arn = each.value.arn
-  provider        = aws.us_east-2
 }
