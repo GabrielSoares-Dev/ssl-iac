33f30a122939bb30b93c17f5057168411688d51d
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-07-02 10:52:54 -0300

    fix:remove validation this

--- certificate.tf
+++ certificate.tf
@@ -4,9 +4,3 @@ resource "aws_acm_certificate" "certificate_examples_subdomains" {
   validation_method = "DNS"
   provider          = aws.us_east-2
 }
-
-resource "aws_acm_certificate_validation" "certificate_examples_validations" {
-  for_each        = aws_acm_certificate.certificate_examples_subdomains
-  certificate_arn = each.value.arn
-  provider        = aws.us_east-2
-}
