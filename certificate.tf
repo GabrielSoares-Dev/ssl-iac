17c693c5884218ac60133d2349abb0eb01a37687
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-07-02 10:33:38 -0300

    feat: change to validation with dns

--- certificate.tf
+++ certificate.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "certificate_examples_subdomains" {
   for_each          = var.example_subdomains
   domain_name       = "${each.value.name}.${var.default_example_domain}"
-  validation_method = "EMAIL"
+  validation_method = "DNS"
   provider          = aws.us_east-2
 }
 
